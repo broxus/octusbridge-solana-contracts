896e76a4d8d50678e76ce80f47389832b950a47d
Alexey Pashinov <pashinov93@gmail.com>
2022-02-18 16:39:55 +0300

    Remove unused packers for custom serialization

--- round-loader/src/lib.rs
+++ round-loader/src/lib.rs
@@ -7,13 +7,11 @@ mod error;
 mod instruction;
 mod processor;
 mod state;
-mod utils;
 
 pub use self::error::*;
 pub use self::instruction::*;
 pub use self::processor::*;
 pub use self::state::*;
-pub use self::utils::*;
 
 #[cfg(not(feature = "no-entrypoint"))]
 mod entrypoint;
