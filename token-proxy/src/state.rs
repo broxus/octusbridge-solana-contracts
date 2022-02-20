81590dce6132412fc56015e8e11d918fcb363546
Alexey Pashinov <pashinov93@gmail.com>
2022-02-21 00:38:34 +0300

    Remove unused TokenKind packer

--- token-proxy/src/state.rs
+++ token-proxy/src/state.rs
@@ -31,15 +31,3 @@ pub enum TokenKind {
     Ever = 0,
     Solana = 1,
 }
-
-pub fn pack_token_kind(kind: TokenKind, dst: &mut [u8; 1]) {
-    *dst = (kind as u8).to_le_bytes()
-}
-
-pub fn unpack_token_kind(src: &[u8; 1]) -> Result<TokenKind, ProgramError> {
-    match u8::from_le_bytes(*src) {
-        0 => Ok(TokenKind::Ever),
-        1 => Ok(TokenKind::Solana),
-        _ => Err(ProgramError::InvalidAccountData),
-    }
-}
